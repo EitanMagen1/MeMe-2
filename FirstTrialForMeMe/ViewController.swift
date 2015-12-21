d04136fdd851db83eb6bd74f5f79e4efac6550da
EitanMagen1 <laptoptech@users-imac.local>
2015-12-21 12:31:44 +0200

    Initial Commit

---
+++ FirstTrialForMeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTrialForMeMe
+//
+//  Created by Lauren Efron on 21/12/2015.
+//  Copyright © 2015 Eitan_Magen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
